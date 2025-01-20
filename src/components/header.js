55481ff4667a844aa3c14cd077d93a22543ff176
Vinicius Fernandes - fxr <vinni.fernandescar@gmail.com>
2025-01-20 17:55:27 -0300

    fix: navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'boxicons/css/boxicons.min.css';
-import '../styles/var.css'; 
-import '../styles/reset.css'; 
+import '../styles/var.css';
+import '../styles/reset.css';
 import '../styles/header.css';
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header>
       <div className="logo">Vinicius Fernandes - dev</div>
-      <nav className="navbar">
+      <nav className={`navbar ${menuActive ? 'active' : ''}`}>
         <a href="#home">Home</a>
         <a href="#about">Sobre</a>
         <a href="#skills">Skills</a>
         <a href="#projects">Projetos</a>
       </nav>
-      <div id="menu-icon" className="bx bx-menu"></div>
+      <div
+        id="menu-icon"
+        className={`bx ${menuActive ? 'bx-x close' : 'bx-menu'}`}
+        onClick={toggleMenu}
+      ></div>
     </header>
   );
 };
