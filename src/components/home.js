f3bcedeb10e3029e8d0862034572abf9b57c0283
Vinicius Fernandes - fxr <vinni.fernandescar@gmail.com>
2025-01-21 18:22:56 -0300

    update: CV

--- src/components/home.js
+++ src/components/home.js
@@ -28,7 +28,7 @@ const Home = () => {
         <h3>Olá, eu me chamo</h3>
         <h1>Vinicius Fernandes</h1>
         <h3>E sou um <span className="multiple-text"></span></h3>
-        <a href="https://docs.google.com/document/d/15elND7SfC2NRDsG8eg7bqu6irD-vbMhd/edit?usp=sharing&ouid=108611119747602290899&rtpof=true&sd=true" target="_blank" rel="noreferrer" className="btn">Download CV <i className='bx bx-arrow-to-bottom'></i></a>
+        <a  href="/Currículo - Vinicius Dev.pdf" download="Currículo - Vinicius Dev.pdf" target="_blank" rel="noreferrer" className="btn">Download CV <i className='bx bx-arrow-to-bottom'></i></a>
         <div className="social-media">
           <a href="https://github.com/fxrnandes" target="_blank" rel="noreferrer"><i className='bx bxl-github'></i></a>
           <a href="https://www.linkedin.com/in/fernandes-vini/" target="_blank" rel="noreferrer"><i className='bx bxl-linkedin'></i></a>
